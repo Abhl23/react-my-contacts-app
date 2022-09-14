db22992eb1441f5d4f305e5ec24c5ed6573b4cb2
Abhi <bhatt23abhishek@gmail.com>
2022-09-14 15:13:33 +0530

    set up import and exports

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import CreateContact from "./CreateContact";
+import ContactCard from "./ContactCard";
+import Loader from './Loader';
+
+export {CreateContact, ContactCard, Loader};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ToastProvider } from "react-toast-notifications";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./styles/index.css";
+import App from "./components/App";
+import ContactsProvider from "./providers/ContactsProvider";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <ToastProvider
+        autoDismiss
+        autoDismissTimeout={5000}
+        placement="top-right"
+      >
+        <ContactsProvider>
+          <App />
+        </ContactsProvider>
+      </ToastProvider>
+    </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
