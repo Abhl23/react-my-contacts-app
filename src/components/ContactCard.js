6a26c79b3eefc9da4daa4267b06e52c5adc6905a
Abhi <bhatt23abhishek@gmail.com>
2022-09-14 15:06:15 +0530

    create ContactCard component

---
+++ src/components/ContactCard.js
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+import { useToasts } from "react-toast-notifications";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser, faArrowUpRightFromSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
+
+import { deleteContact } from "../api";
+import { useContacts } from "../hooks";
+import styles from "../styles/contactcard.module.css";
+
+function ContactCard({ contact }) {
+  const contacts = useContacts();
+
+  const { addToast } = useToasts();
+
+  const handleDeleteContact = async () => {
+    await deleteContact(contact.id);
+
+    contacts.deleteContactFromState(contact.id);
+
+    addToast("Contact Deleted!", {
+      appearance: "success",
+    });
+  };
+
+  return (
+    <div className={styles.contactCard}>
+      <FontAwesomeIcon className={styles.userAvatar} icon={faCircleUser} />
+      <div className={styles.userInfo}>
+        <p>{contact.name}</p>
+        <p>{contact.phone}</p>
+      </div>
+      <div className={styles.contactActions}>
+        <Link to={`/contact-info/${contact.id}`} state={{contact}}>
+          <FontAwesomeIcon className={styles.viewBtn} icon={faArrowUpRightFromSquare} />
+        </Link>
+        <FontAwesomeIcon className={styles.deleteBtn} onClick={handleDeleteContact} icon={faTrash} />
+      </div>
+    </div>
+  );
+}
+
+export default ContactCard;
